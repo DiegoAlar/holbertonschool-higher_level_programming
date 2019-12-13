32b690ad0acf5be4c250f1bb5435e3bbd805f222
DiegoAlar <diegoaav@hotmail.com>
2019-12-13 00:18:49 +0000

    adding exercise 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
             print("{} + {} =\
  {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
         elif (argv[2] == '-'):
-            print("{} + {} =\
+            print("{} - {} =\
  {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
         elif (argv[2] == '*'):
-            print("{} + {} =\
+            print("{} * {} =\
  {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
         elif (argv[2] == '/'):
-            print("{} + {} =\
+            print("{} / {} =\
  {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
