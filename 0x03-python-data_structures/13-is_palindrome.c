1b332e3a769bb5581276af7fbd8adcf13811ff3f
DiegoAlar <diegoaav@hotmail.com>
2019-12-18 05:38:47 +0000

    fixing 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,6 +12,7 @@ int is_palindrome(listint_t **head)
 	int flag = 0;
 	int c = 1;
 	int total = 0;
+	int total2 = 0;
 
 	if ((head == NULL) || (*head == NULL))
 		return (1);
@@ -35,11 +36,11 @@ int is_palindrome(listint_t **head)
 		if (c <= (cont / 2))
 			total += t2->n;
 		else
-			total -= t2->n;
+			total2 += t2->n;
 		c++;
 		t2 = t2->next;
 	}
-	if (total == 0)
+	if ((total - total2) == 0)
 		return (1);
 	else
 		return (0);
