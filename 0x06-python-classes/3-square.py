01718eab5321ec895a8754f0c9910bc7e3659c39
DiegoAlar <diegoaav@hotmail.com>
2020-01-09 18:33:12 -0500

    adding 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """Class that defines instance given size"""
+    def __init__(self, size=0):
+        """init method to initialized variables
+        Args:
+            size (int): the size of the square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """" test """
+    def area(self):
+        return self.__size * self.__size
