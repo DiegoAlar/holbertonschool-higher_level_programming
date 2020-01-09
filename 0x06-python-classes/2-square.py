2a79cebd65756a30432380699851075b60200340
DiegoAlar <diegoaav@hotmail.com>
2020-01-09 17:45:26 -0500

    fixing 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """Class that defines instance given size"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """init method to initialized variables
         Args:
             size (int): the size of the square.
