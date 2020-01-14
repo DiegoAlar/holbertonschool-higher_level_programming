aaf620b1d8a9f63edebfd851f52f18de0e42d64e
DiegoAlar <diegoaav@hotmail.com>
2020-01-14 00:28:30 -0500

    adding changes to exercise 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,24 +6,27 @@ def text_indentation(text):
     """
     i = 0
     flag = True
+    flag2 = False
     if type(text) is not str:
         raise TypeError("text must be a string")
     else:
         while i < len(text):
-            if text[i] is ' ':
+            if text[i] is ' ' and flag2 is False:
                 while flag is True and i < len(text):
                     i += 1
                     if text[i] is not ' ':
                         flag = False
+                        i -= 1
             elif text[i] is '.' or text[i] is '?' or text[i] is ':':
                 print("{:s}\n".format(text[i]))
-                if text[i] is not '.' and text[i] is not '?' and\
-                        text[i] is not ':':
-                    i = i + 1
+                flag2 = True
             else:
-                print(text[i], end="")
-            if flag is False:
-                print(" ", end="")
+                if flag2 is False:
+                    print(text[i], end="")
+                flag2 = False
+            if flag is False and flag2 is False:
                 flag = True
+                print(text[i], end="")
+                i += 1
             else:
                 i += 1
