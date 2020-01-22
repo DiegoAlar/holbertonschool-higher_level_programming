363731a2ef093a98a37ca2bfa9d7ec3dea314269
DiegoAlar <diegoaav@hotmail.com>
2020-01-21 20:07:45 -0500

    fixing 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -15,7 +15,7 @@ if not os.path.exists(filename):
     save_to_json_file(a_list, filename)
 else:
     json_file = load_from_json_file(filename)
-    if type(json_file) is list or type(json_file) is dict:
-        for item in range(1, len(sys.argv)):
+    for item in range(1, len(sys.argv)):
+        if sys.argv[item] not in json_file:
             json_file.append(sys.argv[item])
-        save_to_json_file(json_file, filename)
+    save_to_json_file(json_file, filename)
