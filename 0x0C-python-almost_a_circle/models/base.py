954327d34f4ee1f0f707c2d4756ac427bb766843
DiegoAlar <diegoaav@hotmail.com>
2020-01-27 13:34:17 -0500

    adding exercises up to 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,4 +42,44 @@ class Base:
                 obj_list.append(obj.to_dictionary())
         json_dictionary = cls.to_json_string(obj_list)
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as my_file:
-            return my_file.write(json_dictionary)
+            my_file.write(json_dictionary)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string
+        """
+        import json
+        a_list = []
+        if json_string is None or json_string is "":
+            return a_list
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set
+        """
+        dummy_obj = cls(1, 1, 1, 1)
+        dummy_obj.update(**dictionary)
+        return dummy_obj
+
+    @classmethod
+    def load_from_file(cls):
+        """ Returns a list of instances
+        """
+        import json
+        import os
+
+        if cls.__name__ == "Rectangle":
+            file_path = "Rectangle.json"
+        elif cls.__name__ == "Square":
+            file_patc = "Square.json"
+
+        if os.path.exists(file_path):
+            list_inst = []
+            json_str_to_dict = cls.from_json_string(json.load(file_path))
+            for obj in json_str_to_dict:
+                list_inst.append(cls.create(obj))
+            return list_inst
+        else:
+            return []
