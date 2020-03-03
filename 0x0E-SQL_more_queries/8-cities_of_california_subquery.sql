8396f8de16b04da893e401a5b47a915e82e7ef73
DiegoAlar <diegoaav@hotmail.com>
2020-03-03 16:34:30 -0500

    fixing 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- can be found in the database hbtn_0d_usa.
 SELECT id, name 
 FROM states
-WHERE state_id IN 
+WHERE name = 'California' AND state_id IN 
 	(SELECT state_id 
 	 	FROM cities
 	 	WHERE state_id = 1);
