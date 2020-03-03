4eb1de610639f27634ec14c1dd7adc1f1e218ca4
DiegoAlar <diegoaav@hotmail.com>
2020-03-03 17:09:17 -0500

    fixing 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.id=states.id ORDER BY cities.id ASC;
+INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
