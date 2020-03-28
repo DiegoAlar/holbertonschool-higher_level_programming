bb9f983011321c3f721603b708c9cda66df1cbdf
DiegoAlar <diegoaav@hotmail.com>
2020-03-28 15:30:49 -0500

    fixing task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+""" script that lists all states with a name starting\
+    with N (upper N) from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
 
@@ -13,7 +14,8 @@ if __name__ == "__main__":
             passwd=sys.argv[2],
             db=sys.argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER by id")
+        cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER by id")
         rows = cur.fetchall()
         for col in rows:
             print(col)
