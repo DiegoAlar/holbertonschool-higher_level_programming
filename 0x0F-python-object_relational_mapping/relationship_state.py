e96ba64ed835999c3cb2b9b4e9ca5fbbb1bf9353
DiegoAlar <diegoaav@hotmail.com>
2020-03-28 22:42:50 -0500

    fixing 100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -19,5 +19,4 @@ class State(Base):
     cities = relationship(
         "City",
         back_populates='states',
-        single_parent=True,
-        cascade="all, delete, delete-orphan")
+        cascade=False)
