02cbbecb01591f6e95ea7b6c6eb57b1b2c2ec503
DiegoAlar <diegoaav@hotmail.com>
2020-03-28 22:07:22 -0500

    adding task 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Start link class to table in database\
+   Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    City.states = relationship(
+        "State",
+        order_by=State.id,
+        back_populates="cities")
+    Base.metadata.create_all(engine)
+    cal = State(name='California')
+    cal.cities = [City(name='San Francisco')]
+    session.add(cal)
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""  python file that contains the class definition of\
+     a State and an instance Base = declarative_base():\
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class State(Base):
+    """ class of states column """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        "City",
+        back_populates='states',
+        cascade="all, delete, delete-orphan")
