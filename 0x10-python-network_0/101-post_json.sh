8436e9cea836010e1f68a4625a9b240980419154
DiegoAlar <diegoaav@hotmail.com>
2020-04-09 18:20:34 -0500

    fixing 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -d $2 -H "X-Auth: $TOKEN" $1
+curl -s -d $2 -H "X-Auth: $TOKEN" $1
  
