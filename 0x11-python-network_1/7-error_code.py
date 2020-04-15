cc1d83fa536361dcd2cfbc3bb312cef9f8e1622d
DiegoAlar <diegoaav@hotmail.com>
2020-04-15 12:25:50 -0500

    fixing 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     r = requests.get(url)
-    r.encoding = 'utf-8'
+    # r.encoding = 'utf-8'
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
