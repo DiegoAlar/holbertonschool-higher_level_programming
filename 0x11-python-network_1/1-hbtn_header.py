83006724c2cd55327112d72fd7e7306fc0c70380
DiegoAlar <diegoaav@hotmail.com>
2020-04-15 12:23:24 -0500

    fixing 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,14 +8,7 @@ import urllib.request
 import sys
 
 
-def init():
-    """ Executes the program """
+if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.getheader('X-Request-Id'))
-        print(response.getheaders())
-
-
-if __name__ == "__main__":
-    """ Assures that the module won't be executed if imported """
-    init()
