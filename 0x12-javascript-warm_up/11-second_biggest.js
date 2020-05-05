ab98409efd692de338fdeb56ec6c11f3f72d4d81
DiegoAlar <diegoaav@hotmail.com>
2020-05-05 16:19:54 -0500

    fixing 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,8 @@
 const myArray = process.argv.slice(2);
 
 if (!myArray[0] || myArray.length === 1) {
-  console.log(0);
+  console.log('0');
 } else {
-  console.log('entered');
   myArray.sort(function (a, b) {
     return a - b;
   });
