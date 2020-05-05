3afd06fd19e68d68dcc48056ddcfb7480dd7b37a
DiegoAlar <diegoaav@hotmail.com>
2020-05-05 12:51:20 -0500

    adding task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 var myArgs = process.argv.slice(1);
 if (myArgs.length === 1) {
   console.log('No argument');
+} else if (myArgs.length === 2) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
