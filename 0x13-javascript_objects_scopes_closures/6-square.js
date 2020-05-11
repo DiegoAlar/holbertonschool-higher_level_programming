ac26ca3603c1f9694c6c7b188957e65a13c2d4a0
DiegoAlar <diegoaav@hotmail.com>
2020-05-11 14:20:13 -0500

    adding 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let charPrint;
+    if (c) {
+      charPrint = c;
+    } else {
+      charPrint = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let k = 0; k < this.width; k++) {
+        process.stdout.write(charPrint);
+      }
+      console.log('');
+    }
+  }
+}
+module.exports = Square;
