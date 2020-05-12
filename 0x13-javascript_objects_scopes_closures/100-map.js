a74b4998e40e7000bc187a357cf66cd343d4ee9f
DiegoAlar <diegoaav@hotmail.com>
2020-05-11 19:57:58 -0500

    fixing task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const aList = [];
-const map1 = list.map(function (x) {
-  aList.push(list.indexOf(x) * x);
-});
-
+const map1 = list.map(x => list.indexOf(x) * x);
 console.log(list);
-console.log(aList);
+console.log(map1);
