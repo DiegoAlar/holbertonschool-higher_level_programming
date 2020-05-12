eeefe42ae30564c6401b615429fab21810fbca18
DiegoAlar <diegoaav@hotmail.com>
2020-05-12 12:21:51 -0500

    fixing 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@ const myFile = process.argv.slice(2)[0];
 const fs = require('fs');
 fs.readFile(myFile, 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
