5129df561e786b9f957a90dbdd828b8f35836d01
DiegoAlar <diegoaav@hotmail.com>
2020-05-12 21:09:19 -0500

    adding task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,27 +3,6 @@ const id = process.argv.slice(2)[0];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
 
-// request(url, function (error, response, body) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     const data = JSON.parse(body);
-//     const chars = data.characters;
-//     for (let index = 0; index < chars.length; index++) {
-//       const charUrl = chars[index];
-//       console.log(charUrl);
-//       request(charUrl, function (error, response, body2) {
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           const dataChar = JSON.parse(body2);
-//           console.log(dataChar.name);
-//         }
-//       });
-//     }
-//   }
-// });
-
 function doRequest (url) {
   return new Promise(function (resolve, reject) {
     request(url, function (error, res, body) {
@@ -31,10 +10,6 @@ function doRequest (url) {
         resolve(body);
         const data = JSON.parse(body);
         const chars = data.characters;
-        // for (let index = 0; index < chars.length; index++) {
-        //   const charUrl = chars[index];
-        //   doRequest2(charUrl);
-        // }
         return (chars);
       } else {
         console.log(error);
@@ -61,7 +36,6 @@ function doRequest2 (url) {
 
 async function main () {
   const res = await doRequest(url);
-  // console.log(JSON.parse(res).characters)
   for (let index = 0; index < JSON.parse(res).characters.length; index++) {
     const charUrl = JSON.parse(res).characters[index];
     await doRequest2(charUrl);
